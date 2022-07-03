8b77bde6a0a77880ce0fab8f7088514d04901255
pi@hpi <raspi>
2022-07-03 18:10:38 +0000

    buzzer

---
+++ modules/buzzer/Buzzer.py
@@ -0,0 +1,31 @@
+from gpiozero import TonalBuzzer
+from gpiozero.tones import Tone
+from time import sleep
+
+class Buzzer():
+    def __init__(self, pin):
+        self.tonal = TonalBuzzer(pin)
+
+    @property
+    def is_active(self):
+        return self.tonal.is_active
+
+    def cleanUp(self):
+        self.off()
+
+    def on(self):
+        self.tonal.play(220.0)
+
+    def off(self):        
+        self.tonal.stop()
+
+    def toggle(self):
+        if (self.is_active):
+            self.off()
+        else:
+            self.on()
+
+    def play(self, note, duration):
+        self.tonal.play(Tone(note))
+        sleep(duration)
+        self.tonal.stop()
---
+++ modules/buzzer/app.py
@@ -0,0 +1,145 @@
+from Buzzer import Buzzer 
+import time
+
+russian_anthem = [
+  {'note': "C4",'time': 416},
+
+  {'note': "F4" ,'time': 832},
+  {'note': "C4" ,'time': 624},
+  {'note': "D4" ,'time': 208},
+  {'note': "E4" ,'time': 832},
+  {'note': "A3" ,'time': 416},
+  {'note': "A3" ,'time': 416},
+
+  {'note': "D4" ,'time': 832},
+  {'note': "C4" ,'time': 624},
+  {'note': "A#3" ,'time': 208},
+  {'note': "C4" ,'time': 832},
+  {'note': "F3" ,'time': 416},
+  {'note': "F3" ,'time': 416},
+
+  {'note': "G3" ,'time': 832},
+  {'note': "G3" ,'time': 416},
+  {'note': "A3" ,'time': 416},
+  {'note': "A#3" ,'time': 832},
+  {'note': "A#3" ,'time': 416},
+  {'note': "C4" ,'time': 416},
+
+  {'note': "D4" ,'time': 832},
+  {'note': "E4" ,'time': 416},
+  {'note': "F4" ,'time': 416},
+  {'note': "G4" ,'time': 1664},
+
+  {'note': "A4" ,'time': 832},
+  {'note': "G4" ,'time': 624},
+  {'note': "F4" ,'time': 208},
+  {'note': "G4" ,'time': 832},
+  {'note': "E4" ,'time': 416},
+  {'note': "C4" ,'time': 416},
+
+  {'note': "F4" ,'time': 832},
+  {'note': "E4" ,'time': 624},
+  {'note': "D4" ,'time': 208},
+  {'note': "E4" ,'time': 832},
+  {'note': "A3" ,'time': 416},
+  {'note': "A3" ,'time': 416},
+
+  {'note': "D4" ,'time': 832},
+  {'note': "C4" ,'time': 624},
+  {'note': "A#3" ,'time': 208},
+  {'note': "C4" ,'time': 832},
+  {'note': "F3" ,'time': 624},
+  {'note': "F3" ,'time': 208},
+
+  {'note': "F4" ,'time': 832},
+  {'note': "E4" ,'time': 624},
+  {'note': "D4" ,'time': 208},
+  {'note': "C4" ,'time': 416},
+  {'note': "E4" ,'time': 416},
+  {'note': "F4" ,'time': 416},
+  {'note': "G4" ,'time': 416},
+
+  {'note': "A4" ,'time': 1664},
+  {'note': "G4" ,'time': 416},
+  {'note': "F4" ,'time': 416},
+  {'note': "E4" ,'time': 416},
+  {'note': "F4" ,'time': 416},
+
+  {'note': "G4" ,'time': 1248},
+  {'note': "C4" ,'time': 416},
+  {'note': "C4" ,'time': 416},
+  {'note': "E4" ,'time': 416},
+  {'note': "F4" ,'time': 416},
+  {'note': "G4" ,'time': 416},
+
+  {'note': "F4" ,'time': 1664},
+  {'note': "E4" ,'time': 416},
+  {'note': "D4" ,'time': 416},
+  {'note': "C4" ,'time': 416},
+  {'note': "D4" ,'time': 416},
+
+  {'note': "E4" ,'time': 1248},
+  {'note': "A3" ,'time': 416},
+  {'note': "A3" ,'time': 416},
+  {'note': "C4" ,'time': 416},
+  {'note': "D4" ,'time': 416},
+  {'note': "E4" ,'time': 416},
+
+  {'note': "F4" ,'time': 832},
+  {'note': "D4" ,'time': 624},
+  {'note': "E4" ,'time': 208},
+  {'note': "F4" ,'time': 832},
+  {'note': "D4" ,'time': 624},
+  {'note': "E4" ,'time': 208},
+
+  {'note': "F4" ,'time': 832},
+  {'note': "D4" ,'time': 416},
+  {'note': "F4" ,'time': 416},
+  {'note': "A#4" ,'time': 1664},
+
+  {'note': "A#4" ,'time': 1664},
+  {'note': "A4" ,'time': 416},
+  {'note': "G4" ,'time': 416},
+  {'note': "F4" ,'time': 416},
+  {'note': "G4" ,'time': 416},
+
+  {'note': "A4" ,'time': 1248},
+  {'note': "F4" ,'time': 416},
+  {'note': "F4" ,'time': 1664},
+
+  {'note': "G4" ,'time': 1664},
+  {'note': "F4" ,'time': 416},
+  {'note': "E4" ,'time': 416},
+  {'note': "D4" ,'time': 416},
+  {'note': "E4" ,'time': 416},
+
+  {'note': "F4" ,'time': 1248},
+  {'note': "D4" ,'time': 416},
+  {'note': "D4" ,'time': 1664},
+
+  {'note': "F4" ,'time': 832},
+  {'note': "E4" ,'time': 624},
+  {'note': "D4" ,'time': 208},
+  {'note': "C4" ,'time': 832},
+  {'note': "F3" ,'time': 832},
+
+  {'note': "F4" ,'time': 832},
+  {'note': "E4" ,'time': 624},
+  {'note': "D4" ,'time': 208},
+  {'note': "C4" ,'time': 1248},
+  {'note': "C4" ,'time': 416},
+
+  {'note': "C4" ,'time': 832},
+  {'note': "D4" ,'time': 416},
+  {'note': "E4" ,'time': 416},
+  {'note': "F4" ,'time': 1664}
+]
+
+buzzer = Buzzer(23)
+
+for i in russian_anthem:
+    print(i["note"])
+    try:
+        buzzer.play(i["note"], i['time'] / 1000)
+    except Exception:
+        print(i["note"] + " not working")
