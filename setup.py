9f809e77d153cfe6bdbbd99dd24855a8228cdcec
pi@hpi <raspi>
2022-07-30 15:12:27 +0000

    setup.py to allow npm release of modules

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='myrobotic',
+version='0.1',
+description='My robotic framework for raspberry pi robotics',
+url='#',
+author='Hugo Perier',
+author_email='hugo.perier@protonmail.com',
+license='MIT',
+packages=['modules'],
+zip_safe=False)
