d6236f99fd06bc36c55a8820a85c09d744278104
hugo <hugo.perier@cern.ch>
2022-09-06 21:13:42 +0200

    copy and install confs

--- scripts/install_configuration.sh
+++ scripts/install_configuration.sh
@@ -1,6 +1,30 @@
 #!/bin/sh
 #################################################################
-## Install configuration file in the .myrobotics folder and create
-## it if it does not exist yet
+## Copy all configuration file from the provided folder into the .myrobotics folder
+## and create it if it does not exist yet.
+## Usage: install_configuration.sh <configuration folder>
 #################################################################
 
+$MYROBOTICS_DIR=$HOME/.myrobotics
+
+if [ $# -eq 0 ]
+  then
+    echo "Usage: install_configuration.sh <configuration folder>"
+    exit 1
+fi
+
+# verify that the argument is a folder
+if [ ! -d "$1" ]; then
+  echo "The given argument must be a folder"
+  exit 1
+fi
+
+if [ ! -d $MYROBOTICS_DIR ]; then
+    mkdir $MYROBOTICS_DIR
+fi
+
+cp -r $1/* $MYROBOTICS_DIR
+
+# output files that has been copied
+echo "Configuration files copied:"
+ls $MYROBOTICS_DIR | grep -v ".*\.json"
