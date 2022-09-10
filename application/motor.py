c2010dbd7b5b4cbc6256469e4608730354421ad4
hugo <hugo.perier@cern.ch>
2022-09-10 22:59:26 +0200

    motor via config

--- application/motor.py
+++ application/motor.py
@@ -1,7 +1,26 @@
 from modules.motors.OsoyooMotor import OsoyooMotor
 import time 
 
-def test_motor():
+
+motor1 = {
+    "name": "motor1",
+    "speed": 2000,
+    "pinForward": 27,
+    "pinBackward": 22,
+    "motorSpeedPort": 1
+}
+
+motor2 = {
+    "name": "motor2",
+    "speed": 2000,
+    "pinForward": 23,
+    "pinBackward": 24,
+    "motorSpeedPort": 0
+}
+
+def test_motor(config):
+    print("Testing motor: {}".format(config["name"]))
+    motor = OsoyooMotor(config)
     motors = OsoyooMotor()
     print("move forward")
     motors.forward()
@@ -15,6 +34,8 @@ def test_motor():
     print("stop")  
     motors.stop()
     time.sleep(2.25) 
+    print()
 
 if __name__ == "__main__":
-    test_motor()
+    test_motor(motor1)
+    test_motor(motor2)
--- modules/motors/OsoyooMotor.py
+++ modules/motors/OsoyooMotor.py
@@ -5,13 +5,13 @@ import RPi.GPIO as GPIO
 
 
 class OsoyooMotor:
-    def __init__(self):
+    def __init__(self, configuration):
         self.pwm = Adafruit_PCA9685.PCA9685()
         self.pwm.set_pwm_freq(60)
         GPIO.setmode(GPIO.BCM)
         GPIO.setwarnings(False)
 
-        self.speed = 2000 # Max pulse length out of 4096
+        self.speed = configuration["speed"] # Max pulse length out of 4096
 
         #define L298N(Model-Pi motor drive board) GPIO pins
         # self.IN1 = 23  #Left motor direction pin
@@ -21,9 +21,9 @@ class OsoyooMotor:
         # self.ENA = 0  #Left motor speed PCA9685 port 0
         # self.ENB = 1  #Right motor speed PCA9685 port 1
 
-        self.pinForward = 27
-        self.pinBackward = 22
-        self.motorSpeedPort = 0
+        self.pinForward = configuration["pinForward"]
+        self.pinBackward = configuration["pinBackward"]
+        self.motorSpeedPort = configuration["motorSpeedPort"]
         GPIO.setup(self.pinForward, GPIO.OUT)
         GPIO.setup(self.pinBackward, GPIO.OUT) 
 
