f0e665e0a47192c4c0d200e1f0c9db5a90df5ae9
hugo <hugo.perier@cern.ch>
2022-10-16 17:18:20 +0200

    load configuration from MYROBOTICS_ROOT environment variable

--- scripts/install_environment.sh
+++ scripts/install_environment.sh
@@ -2,3 +2,11 @@
 ## PermissionError: [Errno 13] Permission denied: '/dev/i2c-1'
 ## Permission error that occurs while using adafruit package to control osoyoo motor
 sudo chmod a+rw /dev/i2c-*
+
+## Environment variable checks
+
+# check for MYROBOTICS_ROOT which describe the root directory of the project's configuration path
+if [ -z "$MYROBOTICS_ROOT" ]; then
+    echo "MYROBOTICS_ROOT is not set"
+    exit 1
+fi
