40e10b153511ba03427bcac1268da29cd8e9510e
Hugo Perier <hugo.perier@protonmail.com>
2022-10-16 17:31:14 +0200

    Mrb 8: Point to configuration file using environment variable (#9)
    * load configuration from MYROBOTICS_ROOT environment variable
    
    * install MYROBOTICS_ROOT environnement variable if not set
    
    Co-authored-by: hugo <hugo.perier@cern.ch>
    Co-authored-by: rpi-dev <rpi-dev@hugosoft.fr>

--- modules/utils/func_utils.py
+++ modules/utils/func_utils.py
@@ -1,5 +1,14 @@
 import json
+import os
 
-def load_configuration(path):
-	with open(path) as json_file:
+MYROBOTICS_ROOT = "MYROBOTICS_ROOT"
+
+def load_configuration(path, with_prefix=True):
+	prefix = ""
+	if with_prefix:
+		prefix = os.environ.get(MYROBOTICS_ROOT, "")
+		if (prefix == ""):
+			print("MYROBOTICS_ROOT environment variable not set")
+	file_path = os.path.join(prefix, path)
+	with open(file_path) as json_file:
 		return json.load(json_file)
--- scripts/install_environment.sh
+++ scripts/install_environment.sh
@@ -2,3 +2,16 @@
 ## PermissionError: [Errno 13] Permission denied: '/dev/i2c-1'
 ## Permission error that occurs while using adafruit package to control osoyoo motor
 sudo chmod a+rw /dev/i2c-*
+
+## Environment variable checks
+
+# check for MYROBOTICS_ROOT which describe the root directory of the project's configuration path
+if [ -z "$MYROBOTICS_ROOT" ]; then
+    echo "Setting MYROBOTICS_ROOT"
+    if [ ! -f ~/.bash_profile ]; then
+        echo "Creating bash profile"
+        touch ~/.bash_profile
+    fi
+    echo "export MYROBOTICS_ROOT=$HOME/.myrobotics" >> ~/.bash_profile
+    exit 1
+fi
