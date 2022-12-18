5e5c1cf05750574790422f5f8dd563b9df85938b
hugo <hugo.perier@cern.ch>
2022-12-18 19:38:01 +0100

    move robot based on velocity matrix

--- modules/motors/OsoyooBase.py
+++ modules/motors/OsoyooBase.py
@@ -21,18 +21,33 @@ class OsoyooBase:
         if ((not (-100 <= x <= 100)) or (not (-100 <= yaw <= 100))):
             raise ValueError("Velocity must be define between -100 and 100")
 
-            if x > 0:
-                if yaw > 0:
-                    return [x, max(0, x-yaw)]
-                else:
-                    return [max(0,x+yaw), x]
-            elif x < 0:
-                if yaw > 0:
-                    return [min(0,x+yaw), x]
-                else:
-                    return [x, min(0,x-yaw)]
+        velocities = self.get_velocity_absolute(x, yaw)
+        self.left_wheel.changespeed(abs(velocities[0]) * self.left_wheel.max_speed / 100)
+        if velocities[0] > 0:
+            self.left_wheel.forward()
+        elif velocities[0] < 0:
+            self.left_wheel.backward()
+
+        self.right_wheel.changespeed(abs(velocities[1]) * self.right_wheel.max_speed / 100)
+        if velocities[1] > 0:
+            self.right_wheel.forward()
+        elif velocities[1] < 0:
+            self.right_wheel.backward()
+
+
+    def get_velocity_absolute(self, x, yaw):
+        if x > 0:
+            if yaw > 0:
+                return [x, max(0, x-yaw)]
             else:
-                    return [0, yaw]
+                return [max(0,x+yaw), x]
+        elif x < 0:
+            if yaw > 0:
+                return [min(0,x+yaw), x]
+            else:
+                return [x, min(0,x-yaw)]
+        else:
+            return [0, yaw]
 
     def stop(self):
         for wheel in self.wheels:
