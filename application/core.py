94a43a99148dd466d3e5916659e961a7b25d01af
hugo <hugo.perier@cern.ch>
2023-12-29 11:37:25 +0100

    Add CLI for core

--- application/core.py
+++ application/core.py
@@ -1,14 +1,84 @@
+import argparse
 from modules.utils.func_utils import load_configuration
 from modules.utils.threaded_http_server import ThreadedHTTPServer
 from modules.core.CoreHttpHandler import CoreHttpHandler
 from modules.core.CoreModule import Core
+import requests
+import time
+
+def run_server(configuration):
+    port = configuration.get("port")
+    server = ThreadedHTTPServer(('0.0.0.0', port), CoreHttpHandler)
+    server.configuration = configuration
+    server.core = Core(configuration=configuration)
+    server.core.enable_status_thread()
+    server.serve_forever()
+
+def custom_link_function():
+    core = Core(configuration=configuration)
+    key = input("Enter the key: ")
+    if (len(key) != 36):
+        print("Invalid key")
+        return
+    
+    url = f"{configuration.get('neutronCoreUri')}/agent/link"
+    response = requests.post(url, {"secretKey": key})
+
+    if response.status_code == 200:
+        core.configuration["secretKey"] = key
+        core.configuration["linked"] = True
+        core.update_configuration()
+        print("Linking successful")
+    else:
+        print(f"Error linking: {response.text}")
+
+def custom_status_function(configuration):
+    core = Core(configuration=configuration)
+
+    serverUri = 'http://localhost:' + str(configuration.get('port')) + '/robot/status'
+    status = "Online"
+    try:
+        print(serverUri)
+        response = requests.get(serverUri)
+        if (response.status_code != 200):
+            status = "Unknown"
+            return
+        modules = response.json().get("modules")
+        if (modules != []):
+            status = "Operating"
+    except:
+        status = "Offline"
+
+    systemInformations = {
+        "status": status,
+        "battery": core.get_battery(),
+        "system": {
+            "cpu": core.cpu_usage,
+            "memory": core.memory_usage,
+        },
+        "location": {
+            "name": core.get_location()
+        }
+    }
+    print(systemInformations)
 
 if __name__ == '__main__':
-	print("starting forever")
-	configuration = load_configuration("core.json")
+    parser = argparse.ArgumentParser(description='The neutron agent is responsible for installing and managing the services for the robot.')    
 
-	port = configuration.get("port")
-	server = ThreadedHTTPServer(('0.0.0.0', port), CoreHttpHandler)
-	server.configuration = configuration
-	server.core = Core(configuration=configuration)
-	server.serve_forever()
+    parser.add_argument('--run', action='store_true', help='Run the server')
+    parser.add_argument('--link', action='store_true', help='Link the robot with the Neutron platform')
+    parser.add_argument('--status', action='store_true', help='Display robot status')
+
+    args = parser.parse_args()
+    configuration = load_configuration("core.json")
+
+    if args.run:
+        print("Starting server forever")
+        run_server(configuration)
+    elif args.link:
+        custom_link_function()
+    elif args.status:
+        custom_status_function(configuration)
+    else:
+        parser.print_help()
+    exit(0)
--- modules/core/CoreModule.py
+++ modules/core/CoreModule.py
@@ -2,6 +2,10 @@ from modules.utils.ProcessManager import ProcessManager
 from modules.utils.func_utils import load_configuration, get_time
 import psutil
 import os
+import json
+import requests
+import threading
+import time
 
 class Core:
     def __init__(self, configuration):
@@ -20,6 +24,12 @@ class Core:
         self.process_manager = ProcessManager()
         self.process_manager.start()
         self.operation_time = -1
+        self.stop_event = threading.Event()
+
+
+    def __del__(self):
+        self.process_manager.stop()
+        self.stop_status_thread()
 
     def make_process(self, processId):
         processInfos = [x for x in self.configuration.get(
@@ -48,6 +58,87 @@ class Core:
             "type"), "framePackage": x.get("framePackage"), "configuration": x.get("configuration")}, modules)
         return modules
 
+    def get_battery(self):
+        battery = {
+            "level": 80,
+            "charging": False
+        }
+        return battery
+
+    def enable_status_thread(self):
+        # Create and start a thread that runs send_robot_status every 5 minutes
+        self.status_thread = threading.Thread(target=self.status_thread_function)
+        self.status_thread.start()
+
+    def status_thread_function(self):
+        while not self.stop_event.is_set():
+            # Perform the status update
+            self.send_robot_status()
+
+            # Sleep for 5 minutes
+            time.sleep(300)  # 300 seconds = 5 minutes
+
+    def stop_status_thread(self):
+        # Set the stop event to signal the thread to stop
+        self.stop_event.set()
+
+        # Wait for the thread to finish
+        self.status_thread.join()
+
+    def send_robot_status(self):
+        if (self.configuration.get("linked") != True):
+            print("Error: The robot must be linked to send status to the server")
+            return
+
+        serverUri = str(self.configuration.get('neutronCoreUri')) + '/agent/publishSystemInformation'
+        systemInformations = {
+                "status": self.status,
+                "battery": self.get_battery(),
+                "system": {
+                    "cpu": self.cpu_usage,
+                    "memory": self.memory_usage,
+                },
+                "location": {
+                    "name": self.get_location()
+                }
+            }
+        requestPayload = {
+            "secretKey": self.configuration.get("secretKey"),
+            "status": systemInformations
+        }
+
+        try:
+            response = requests.post(serverUri, json=requestPayload)
+            print("Published status to neutron server")
+            if (response.status_code != 200):
+                print("Error while publishing robot status")
+                print(response.text)
+        except Exception as e:
+            print("[CORE] Error sending robot status")
+            print(str(e))
+        
+
+    def update_configuration(self):
+        try:
+            myrobotics_root = os.environ.get("MYROBOTICS_ROOT")
+            if not myrobotics_root:
+                raise ValueError("MYROBOTICS_ROOT environment variable not set")
+
+            config_path = os.path.join(myrobotics_root, "core.json")
+
+            with open(config_path, "w") as config_file:
+                json.dump(self.configuration, config_file, indent=4)
+
+            print(f"[CORE] Configuration updated and saved to {config_path}")
+
+        except Exception as e:
+            print("[CORE] Error updating configuration")
+            print(str(e))
+
+
+    def get_location(self):
+        return "Unknown"
+
     def get_processes_status(self):
         processes = []
         for i in self.process_manager.processes:
@@ -65,5 +156,5 @@ class Core:
     @property
     def status(self):
         if (len(self.process_manager.processes)):
-            return "Connected"
-        return "Available"
+            return "Operating"
+        return "Online"
